87eac3875e66ca20e631473146ab3d3c62534c19
Damian Szmurlo <damianszmurlowork@gmail.com>
2023-08-13 00:40:57 +0200

    final

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const AframeScene = () => {
   return (
     <a-scene>
       <Lights />
-      <a-light position="0 2 0" />
+      <a-light id="main" target="#box" position="0 10 -15" />
       <a-box id="box" position="0 1 -17" roughness="1" metalness="1" ></a-box>
       <a-plane id="floor" opacity="0.6" rotation="-90 0 0" width="1000" height="1000" repeat="2000 2000" src={floor} roughness="0" metalness="1" />
       <a-sky color="black"></a-sky>
--- src/components/Lights.js
+++ src/components/Lights.js
@@ -7,8 +7,8 @@ const Lights = () => {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    const lightConfigurations = Array(19).fill(0).map(() => ({
-        position: [getRandomPosition(-500, 500), 240, getRandomPosition(-500, 500)],
+    const lightConfigurations = Array(36).fill(0).map(() => ({
+        position: [getRandomPosition(-500, 500), 100, getRandomPosition(-17, 100)],
         duration: getRandomPosition(3000, 5000),
         color: "#0696FF"
     }));
@@ -20,9 +20,8 @@ const Lights = () => {
                     key={index}
                     color={config.color}
                     position={`${config.position[0]} ${config.position[1]} ${config.position[2]}`}
-                    intensity="0.05"
-                    type="point"
-                    animation={`property: position; to: 0 0.2 -17; dur: ${config.duration}; loop: true; easing: easeOutQuad;`}>
+                    intensity="0.005"
+                    animation={`property: position; to: 0 100 -200; dur: ${config.duration}; loop: true; easing: easeOutQuad;`}>
                 </a-light>
             ))}
         </>
